c8577c767a2b908e04fa4e57d778bb159d08bd9e
derickmulder@gmail.com <derickmulder@gmail.com>
2023-07-20 21:39:44 +0000

    Edited settings and mapperHome.oneToOneSetup

--- client_code/mapperHome/oneToOneSetup/__init__.py
+++ client_code/mapperHome/oneToOneSetup/__init__.py
@@ -10,18 +10,26 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class oneToOneSetup(oneToOneSetupTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Get User
-    user = anvil.users.get_user()
-    # Any code you write here will run before the form opens.
-    sheet_data = anvil.server.call('getSheetData', user)
-    self.oneToOneSourceSheetDropDown.items = [sheet['sheet_name'] for sheet in sheet_data]
+        # Get User
+        user = anvil.users.get_user()
+        # Any code you write here will run before the form opens.
+        sheet_data = anvil.server.call('getSheetData', user)
+        # Create a dictionary to map sheet names to sheet IDs
+        self.sheet_map = {sheet['sheet_name']: sheet['sheet_id'] for sheet in sheet_data}
+        # Set the dropdown items to be the sheet names
+        self.oneToOneSourceSheetDropDown.items = list(self.sheet_map.keys())
 
-
-
-
-
-
+        # Set the event handler for the dropdown's change event
+        self.oneToOneSourceSheetDropDown.set_event_handler('change', self.oneToOneSourceSheetDropDown_change)
+    
+    def oneToOneSourceSheetDropDown_change(self, **event_args):
+        """This method is called when an item is selected"""
+        selectedSheetName = self.oneToOneSourceSheetDropDown.selected_value
+        
+        if selectedSheetName is not None:
+            selectedSheetId = self.sheet_map[selectedSheetName]
+            print(f"Selected Sheet ID: {selectedSheetId}")
