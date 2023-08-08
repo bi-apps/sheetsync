6dc7550410918d50e896eb998e8085e870b272e5
derickmulder@gmail.com <derickmulder@gmail.com>
2023-08-08 23:16:38 +0000

    Edited mapperHome.criteriaBasedOneToOneSetup

--- client_code/mapperHome/criteriaBasedOneToOneSetup/__init__.py
+++ client_code/mapperHome/criteriaBasedOneToOneSetup/__init__.py
@@ -262,6 +262,7 @@ class criteriaBasedOneToOneSetup(criteriaBasedOneToOneSetupTemplate):
         # Additional behavior for list_value_elements
         if self.selected_operator_values in ["is_one_of", "is_not_one_of"]:
              # self.oneToOneCriteriaBasedOperatorIsOneOfOrNotLabel.text = "These Values"
+             self.oneToOneCriteriaBasedMultiSelectDropDown
              logical_criteria_columns_data = anvil.server.call('getColumnNames', self.selected_criteria_source_sheet_id, self.user)
              self.logical_criteria_column_map = {column['title']: column['id'] for column in logical_criteria_columns_data}
             
