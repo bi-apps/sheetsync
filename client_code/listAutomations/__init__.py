7263b039673ffbc9d1dd4b8d25551dbd4354f21c
derickmulder@gmail.com <derickmulder@gmail.com>
2023-08-12 01:04:22 +0000

    Created listAutomations.ItemTemplate4; Edited listAutomations and listAutomations.RowTemplate8

--- client_code/listAutomations/__init__.py
+++ client_code/listAutomations/__init__.py
@@ -22,6 +22,21 @@ class listAutomations(listAutomationsTemplate):
 
         # Get User Specific Data -----
         # Get logged in User Object or return to home if not logged in
-        user = anvil.users.get_user()
+        self.user = anvil.users.get_user()
         # if not user:
         #   open_form('homePage')
+
+    def flow_panel_2_show(self, **event_args):
+        """This method is called when the FlowPanel is shown on the screen"""
+        self.repeatingPanelMain.items = tables.app_tables.tb_automation_type_1_2.search(user=self.user)
+
+    def addMappingButton_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        open_form('mapperHome')
+
+    def data_row_panel_1_show(self, **event_args):
+        """This method is called when the data row panel is shown on the screen"""
+        tables.app_tables.tb_automation_type_1_2.search(user=self.user)
+
+
+
