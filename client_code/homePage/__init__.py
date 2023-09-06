5751d8ac3ce31913adffbf4b91dd94c3f083df73
derickmulder@gmail.com <derickmulder@gmail.com>
2023-09-06 21:29:20 +0000

    Created homePage_backup.RowTemplate1, homePage_backup.ItemTemplate1 and homePage_backup; Edited settings and homePage; Deleted homePage_copy, homePage.RowTemplate1 and homePage.ItemTemplate1

--- client_code/homePage/__init__.py
+++ client_code/homePage/__init__.py
@@ -12,45 +12,39 @@ import anvil.users
 
 
 class homePage(homePageTemplate):
-  def __init__(self, **properties):
-    self.init_components(**properties)
-    # Delay the check using a timer
-    is_logged_in = anvil.server.call('check_logged_in')
-    if is_logged_in:
-        open_form('indexPage', user=anvil.users.get_user())
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        # Delay the check using a timer
+        is_logged_in = anvil.server.call('check_logged_in')
+        if is_logged_in:
+            open_form('indexPage', user=anvil.users.get_user())
 
 
-    # # If a user is already logged in, open the index page
-    # logged_in_user = anvil.users.get_user()
-    # if logged_in_user:
-    #    open_form('indexPage', user=logged_in_user)
+        # # If a user is already logged in, open the index page
+        # logged_in_user = anvil.users.get_user()
+        # if logged_in_user:
+        #    open_form('indexPage', user=logged_in_user)
 
-  def signIn_user_click_event(self, **event_args):
-      """This method is called when the button is clicked"""
-      try:
-         anvil.users.login_with_form(show_signup_option=False, allow_cancel=True, remember_by_default=True)
-         # Directly open the index page after login
-         open_form('indexPage')
-      except anvil.users.AuthenticationFailed:
-         # If login fails, just return and stay on the home page
-         return
-
-
-    # If login succeeds, open the index page
-    # logged_in_user = anvil.users.get_user()
-    # open_form('indexPage', user=logged_in_user)
-
-  def signup_user_click_event(self, **event_args):
-    """This method is called when the button is clicked"""
-    anvil.users.signup_with_form(allow_cancel=True)
-    # After signup, check if a user is logged in
-    logged_in_user = anvil.users.get_user()
-    if logged_in_user:
-      # If a user is logged in, open the index page
-      open_form('indexPage', user=logged_in_user)
+    def signIn_user_click_event(self, **event_args):
+        """This method is called when the button is clicked"""
+        try:
+            anvil.users.login_with_form(show_signup_option=False, allow_cancel=True, remember_by_default=True)
+            # Directly open the index page after login
+            open_form('indexPage')
+        except anvil.users.AuthenticationFailed:
+            # If login fails, just return and stay on the home page
+            return
 
 
+        # If login succeeds, open the index page
+        # logged_in_user = anvil.users.get_user()
+        # open_form('indexPage', user=logged_in_user)
 
-
-
-
+    def signup_user_click_event(self, **event_args):
+        """This method is called when the button is clicked"""
+        anvil.users.signup_with_form(allow_cancel=True)
+        # After signup, check if a user is logged in
+        logged_in_user = anvil.users.get_user()
+        if logged_in_user:
+        # If a user is logged in, open the index page
+            open_form('indexPage', user=logged_in_user)
